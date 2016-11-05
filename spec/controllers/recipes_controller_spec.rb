84d4d1f91268bb237ea65cbcd81cea83ccc0df0e
Kristine <kteich88@gmail.com>
2016-11-05 13:09:29 -0400

    added recipe controller spec.

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,2 +1,17 @@
 RSpec.describe RecipesController do
+  render_views
+  let(:json) { JSON.parse(response.body) }
+
+  describe 'GET /api/recipe/:id' do
+    before do
+      response = get :show, format: :json, params: { 'id' => 1 }
+    end
+
+    context 'when a recipe exists' do
+      it 'returns a status code 200' do
+        # need to somehow include the auth token here...
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
