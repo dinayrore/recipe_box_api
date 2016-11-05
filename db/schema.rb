6f36f5889ffa562c0c3a68bfa614f05b3335b327
Kristine <kteich88@gmail.com>
2016-11-04 21:13:51 -0400

    generated new table in schema file with new migration for recipes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "name"
+    t.string  "ingredients"
+    t.string  "image"
+    t.integer "yield"
+    t.string  "instructions"
+    t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 1) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
