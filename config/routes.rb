4dab7f944bd139d8e2e456883fd22f49f5f0f8f7
Kristine <kteich88@gmail.com>
2016-11-04 21:11:04 -0400

    Updated routes with recipe CRUD functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
 
+  get '/api/recipes', to: 'recipes#index' # all recipe data
+  get '/api/recipe/:id', to: 'recipes#show' # specific recipe
+  post '/api/recipes', to: 'recipes#create' # create a new recipe
+  patch '/api/recipe/:id', to: 'recipes#update' # edit recipe data
+  delete '/api/recipe/:id', to: 'recipes#destroy' # delete recipe data
+
+  resources :recipes
 end
